44ebc7f92e093427c726872a6a4d3664190a7a8d
Bart Moelans <bart@moelans.be>
2019-01-18 15:31:04 +0100

    Split of calculating of index in seperate function

--- wordfreq.c
+++ wordfreq.c
@@ -18,15 +18,22 @@
 #include <string.h>    /* for "strtok" */
 #include <ctype.h>    /* for "toupper" */
 
-void read_input(char *var_name) {
+/* Macro to create/fill-in a ydb_buffer_t structure from ydb_string_t
+ */
+#define YDB_STRING_TO_BUFFER(STRING, BUFFERP)					\
+{										\
+	(BUFFERP)->buf_addr = (STRING)->address;						\
+	(BUFFERP)->len_used = (BUFFERP)->len_alloc = (STRING)->length;	\
+}
+
+void read_input(ydb_string_t *var_name) {
     ydb_buffer_t tmp_b, index, words;
     ydb_buffer_t value;
     char valuebuff[64], linebuff[1024], *lineptr, *ptr, *tmp_ptr;
     int status;
 
-    (&words)->buf_addr = var_name;
-    (&words)->len_used = (&words)->len_alloc = strlen(var_name);
-    if (var_name[0] == '^') {
+    YDB_STRING_TO_BUFFER(var_name, &words);
+    if (var_name->address[0] == '^') {
         ydb_delete_s(&words, 0, NULL, YDB_DEL_TREE);
     }
 
@@ -60,29 +67,17 @@ void read_input(char *var_name) {
     } while (1);
 }
 
-/* count and report word frequencies for http://www.cs.duke.edu/csed/code/code2007/ */
-int main() {
-    char *var_name;
-    var_name = malloc(10 * sizeof(char));
-    if (1) {
-        strcpy(var_name, "words");
-    } else {
-        strcpy(var_name, "^words");
-    }
-
-    ydb_buffer_t tmp1, tmp2, index, words, words_tmp1, null;
+ydb_status_t create_word_index(ydb_string_t *var_name, ydb_string_t *index_name) {
+    ydb_buffer_t tmp_b, index, words, words_tmp1, null;
     ydb_buffer_t index_subscr[2];
-    char tmp1buff[64], tmp2buff[64], words_tmp1buff[64];
+    char tmp1buff[64], words_tmp1buff[64];
     int status;
+    YDB_STRING_TO_BUFFER(var_name, &words);
+    YDB_STRING_TO_BUFFER(index_name, &index);
 
-    read_input(var_name);
-    (&words)->buf_addr = var_name;
-    (&words)->len_used = (&words)->len_alloc = strlen(var_name);
-
-    YDB_LITERAL_TO_BUFFER("index", &index);
-    tmp1.buf_addr = tmp1buff;            /* M line : set tmp1="" */
-    tmp1.len_used = 0;
-    tmp1.len_alloc = sizeof(tmp1buff);
+    tmp_b.buf_addr = tmp1buff;            /* M line : set tmp1="" */
+    tmp_b.len_used = 0;
+    tmp_b.len_alloc = sizeof(tmp1buff);
     words_tmp1.buf_addr = &words_tmp1buff[0];
     words_tmp1.len_used = 0;
     words_tmp1.len_alloc = sizeof(words_tmp1buff);
@@ -90,18 +85,55 @@ int main() {
     null.len_used = 0;
     null.len_alloc = 0;
     do {
-        status = ydb_subscript_next_s(&words, 1, &tmp1, &tmp1);    /* M line : set tmp1=$order(words(tmp1)) */
+        status = ydb_subscript_next_s(&words, 1, &tmp_b, &tmp_b);    /* M line : set tmp1=$order(words(tmp1)) */
         if (YDB_ERR_NODEEND == status)
             break;
         YDB_ASSERT(YDB_OK == status);
-        YDB_ASSERT(0 != tmp1.len_used);
-        status = ydb_get_s(&words, 1, &tmp1, &words_tmp1);    /* M line : set words_tmp1=words(tmp1) */
+        YDB_ASSERT(0 != tmp_b.len_used);
+        status = ydb_get_s(&words, 1, &tmp_b, &words_tmp1);    /* M line : set words_tmp1=words(tmp1) */
         YDB_ASSERT(YDB_OK == status);
         index_subscr[0] = words_tmp1;
-        index_subscr[1] = tmp1;
+        index_subscr[1] = tmp_b;
         status = ydb_set_s(&index, 2, index_subscr, &null);    /* M line : set index(words_tmp1,tmp1)="" */
         YDB_ASSERT(YDB_OK == status);
     } while (1);
+    return YDB_OK;
+}
+
+
+/* count and report word frequencies for http://www.cs.duke.edu/csed/code/code2007/ */
+int main() {
+    ydb_string_t *var_name, *index_name;
+
+    var_name = malloc(sizeof(ydb_string_t));
+    var_name->address = malloc(10 * sizeof(char));
+    index_name = malloc(sizeof(ydb_string_t));
+    index_name->address = malloc(10 * sizeof(char));
+    if (1) {
+        strcpy(var_name->address, "words");
+        var_name->length = 5;
+    } else {
+        strcpy(var_name->address, "^words");
+        var_name->length = 6;
+    }
+    strcpy(index_name->address, "index");
+    index_name->length = 5;
+
+    ydb_buffer_t tmp1, tmp2, index, words;
+    ydb_buffer_t index_subscr[2];
+    char tmp1buff[64], tmp2buff[64];
+    int status;
+
+    read_input(var_name);
+    YDB_STRING_TO_BUFFER(var_name, &words);
+    YDB_STRING_TO_BUFFER(index_name, &index);
+
+    create_word_index(var_name, index_name);
+
+    tmp1.buf_addr = tmp1buff;            /* M line : set tmp1="" */
+    tmp1.len_used = 0;
+    tmp1.len_alloc = sizeof(tmp1buff);
+
     do {
         status = ydb_subscript_previous_s(&index, 1, &tmp1, &tmp1);    /* M line : set tmp1=$order(index(tmp1),-1) */
         if (YDB_ERR_NODEEND == status)
@@ -126,6 +158,7 @@ int main() {
             index_subscr[1] = tmp2;
         } while (1);
     } while (1);
+    free(index_name);
     free(var_name);
     return YDB_OK;
 }
